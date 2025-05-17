c26c69fab00b34a5f80289d2659de3ae0d8caebb
Helloyunho <me@helloyunho.xyz>
2025-05-17 17:34:58 +0900

    feat: make pill text input

---
+++ src/components/pill-text-input.stories.tsx
@@ -0,0 +1,74 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import {
+  PillTextInput,
+  PillTextInputIcon,
+  PillTextInputRoot,
+  PillTextInputRootProps
+} from './pill-text-input'
+import { Search } from 'lucide-react'
+
+const meta: Meta<typeof PillTextInputRoot> = {
+  component: PillTextInputRoot,
+  subcomponents: {
+    PillTextInputIcon,
+    PillTextInput
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof PillTextInputRoot>
+
+export const TextInputWithIcon: Story & {
+  args: {
+    pos?: 'left' | 'right'
+    placeholder?: string
+  }
+  argTypes: {
+    pos: {
+      options: ['left', 'right']
+      control: { type: 'select' }
+    }
+  }
+} = {
+  args: {
+    placeholder: 'Search...',
+    pos: 'left'
+  },
+  render: ({
+    pos,
+    placeholder
+  }: PillTextInputRootProps & {
+    pos?: 'left' | 'right'
+    placeholder?: string
+  }) => (
+    <PillTextInputRoot>
+      <PillTextInputIcon pos={pos}>
+        <Search className="w-6 h-6" />
+      </PillTextInputIcon>
+      <PillTextInput placeholder={placeholder} />
+    </PillTextInputRoot>
+  ),
+  argTypes: {
+    pos: {
+      options: ['left', 'right'],
+      control: { type: 'select' }
+    }
+  }
+}
+
+export const TextInputWithoutIcon: Story & {
+  args: {
+    placeholder?: string
+  }
+} = {
+  args: {
+    placeholder: 'Search...'
+  },
+  render: ({
+    placeholder
+  }: PillTextInputRootProps & { placeholder?: string }) => (
+    <PillTextInputRoot>
+      <PillTextInput placeholder={placeholder} />
+    </PillTextInputRoot>
+  )
+}
---
+++ src/components/pill-text-input.tsx
@@ -0,0 +1,67 @@
+import { cn } from '@/utils/cn'
+import { Slot } from '@radix-ui/react-slot'
+import React from 'react'
+
+export interface PillTextInputRootProps extends React.ComponentProps<'div'> {}
+
+export function PillTextInputRoot({
+  children,
+  className,
+  ...props
+}: PillTextInputRootProps) {
+  return (
+    <div
+      className={cn(
+        'relative text-secondary focus-within:text-secondary-hover',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
+
+export function PillTextInputIcon({
+  children,
+  className,
+  pos = 'left'
+}: {
+  children: React.ReactNode
+  className?: string
+  pos?: 'left' | 'right'
+}) {
+  return (
+    <Slot
+      role="presentation"
+      className={cn(
+        'absolute top-3 pointer-events-none size-5',
+        pos === 'left' ? 'left-4 [&~input]:pl-11' : 'right-4',
+        className
+      )}
+    >
+      {children}
+    </Slot>
+  )
+}
+
+export interface PillTextInputProps
+  extends Omit<React.ComponentProps<'input'>, 'type'> {
+  icon?: React.ReactNode
+}
+
+export function PillTextInput({ className, ...props }: PillTextInputProps) {
+  const ref = React.useRef<HTMLInputElement>(null)
+
+  return (
+    <input
+      className={cn(
+        'rounded-full w-full h-13 px-4 border-2 border-solid border-white placeholder-shown:border-secondary bg-transparent text-base font-bold text-black transition-colors placeholder:text-secondary focus:border-white focus:placeholder:text-secondary-hover disabled:cursor-not-allowed focus-visible:outline-0',
+        className
+      )}
+      type="text"
+      ref={ref}
+      {...props}
+    />
+  )
+}
