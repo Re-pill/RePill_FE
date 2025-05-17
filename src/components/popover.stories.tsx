ce6fd8acb981cd1f351aaf0430b31a7a747dfc2a
Helloyunho <me@helloyunho.xyz>
2025-05-17 23:58:14 +0900

    feat: add popover from shadcn
    ...with some minor color change

---
+++ src/components/popover.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import {
+  Popover as PopoverComp,
+  PopoverContent,
+  PopoverTrigger
+} from './popover'
+import type { PopoverProps } from '@radix-ui/react-popover'
+import { PillButton } from './pill-button'
+
+const meta: Meta<typeof PopoverComp> = {
+  component: PopoverComp,
+  subcomponents: {
+    PopoverContent,
+    PopoverTrigger
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof PopoverComp>
+
+export const Popover: Story & {
+  args: {
+    triggerText?: string
+    contentText?: string
+  }
+} = {
+  args: {
+    triggerText: 'Click me',
+    contentText: 'Hello world'
+  },
+  render: ({
+    triggerText,
+    contentText
+  }: PopoverProps & {
+    triggerText?: string
+    contentText?: string
+  }) => (
+    <PopoverComp>
+      <PopoverTrigger>
+        <PillButton>{triggerText}</PillButton>
+      </PopoverTrigger>
+      <PopoverContent>{contentText}</PopoverContent>
+    </PopoverComp>
+  )
+}
