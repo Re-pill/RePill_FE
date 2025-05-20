890ace9655472bbff5a8457c5f8eba5b245ae99b
Helloyunho <me@helloyunho.xyz>
2025-05-20 13:03:57 +0900

    refactor: address page just got better

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -2,114 +2,152 @@
 
 import { useState } from 'react'
 import Image from 'next/image'
-import { CheckIcon, LocationEditIcon, SearchIcon } from 'lucide-react'
+import { Locate, SearchIcon } from 'lucide-react'
 import {
   PillTextInput,
   PillTextInputIcon,
   PillTextInputRoot
 } from '@/components/pill-text-input'
 import { PillButton } from '@/components/pill-button'
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogFooter,
+  AlertDialogAction
+} from '@/components/alert-dialog'
 
 export default function AddressPage () {
   const [inputValue, setInputValue] = useState('')
-  const [isRegistered, setIsRegistered] = useState(false)
+  const [open, setOpen] = useState(false)
 
   const handleRegister = () => {
-    if (inputValue) {
-      setIsRegistered(true)
-      setTimeout(() => setIsRegistered(false), 2000)
+    // TODO: Actually register the address
+    setOpen(true)
+  }
+
+  const onClickCurrentLocation = () => {
+    // TODO: Send the coordinate to server and get the actual address
+    const success = (pos: GeolocationPosition) => {
+      setInputValue(`${pos.coords.latitude} ${pos.coords.longitude}`)
     }
+    const error = (error: unknown) => {
+      console.error(error)
+    }
+    navigator.geolocation.getCurrentPosition(success, error, {
+      timeout: 5000,
+      maximumAge: 0
+    })
   }
 
   return (
-    <div className='relative'>
-      {/* 강조 배경 */}
-      <div className='absolute top-[3.75rem] left-[5.625rem] w-[5rem] h-[5rem] rounded-full bg-[radial-gradient(ellipse_at_center,orange_0%,_transparent_70%)] z-0' />
+    <>
+      <div className='relative h-screen'>
+        <div
+          className='absolute top-26 left-18 w-20 h-20 rounded-full -z-10' style={{
+            background: 'radial-gradient(50% 50% at 50% 50%, #FF7447 0%, transparent 100%)'
+          }}
+        />
 
-      {/* 상단 문구 */}
-      <div className='pt-20 px-6 z-10'>
-        <p className='text-xl font-bold text-black leading-tight flex items-center gap-2'>
-          반가워요!
-          <Image
-            src='/images/logo.svg'
-            alt='Re pill logo'
-            width={28}
-            height={28}
-            priority
-          />
-        </p>
-        <p className='text-xl font-bold text-black leading-tight'>
-          가까운 수거함을 찾으려면
-          <br /> 주소가 필요해요.
-        </p>
-      </div>
+        <div className='flex flex-col h-full justify-between py-32'>
+          <div className='flex flex-col gap-8'>
+            <div>
+              <div className='flex items-center gap-2'>
+                <span className='text-2xl font-bold text-black'>
+                  반가워요!
+                </span>
+                <Image
+                  src='/images/logo.svg'
+                  alt='Re pill logo'
+                  width={28}
+                  height={28}
+                  priority
+                />
+              </div>
+              <p className='text-2xl font-bold text-black'>
+                가까운 수거함을 찾으려면
+                <br /> 주소가 필요해요.
+              </p>
+            </div>
 
-      {/* 입력창 및 위치 설정 */}
-      <div className='px-6 pt-8 z-10'>
-        <PillTextInputRoot>
-          <PillTextInput
-            placeholder='도로명, 건물명, 지번 입력'
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <PillTextInputIcon pos='right'>
-            <SearchIcon className='text-secondary-bg-hover' />
-          </PillTextInputIcon>
-        </PillTextInputRoot>
+            <div>
+              <PillTextInputRoot>
+                <PillTextInput
+                  placeholder='도로명, 건물명, 지번 입력'
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                <PillTextInputIcon pos='right'>
+                  <SearchIcon className='w-6 h-6' />
+                </PillTextInputIcon>
+              </PillTextInputRoot>
 
-        {inputValue.trim() === '' && (
-          <div className='flex justify-end pt-0'>
+              {inputValue.trim() === ''
+                ? (
+                  <div className='flex justify-end'>
+                    <PillButton
+                      variant='link'
+                      size='sm'
+                      className='text-xs font-medium text-orange hover:text-orange gap-1'
+                      onClick={() => {
+                        onClickCurrentLocation()
+                      }}
+                    >
+                      <Locate className='w-4 h-4' />
+                      현재 위치로 설정
+                    </PillButton>
+                  </div>
+                  )
+                : (
+                  <div className='py-4 space-y-3 text-sm px-4'>
+                    <p className='text-black font-semibold'>입력예시</p>
+                    <div>
+                      <p className='text-secondary font-semibold'>도로명 + 건물번호</p>
+                      <p className='text-orange font-semibold'>역삼로 180</p>
+                    </div>
+                    <div>
+                      <p className='text-secondary font-semibold'>지역명(동/리)+번지</p>
+                      <p className='text-orange font-semibold'>강남구 역삼동 790-6</p>
+                    </div>
+                    <div>
+                      <p className='text-secondary font-semibold'>지역명(동/리)+건물명</p>
+                      <p className='text-orange font-semibold'>역삼동 마루 180</p>
+                    </div>
+                  </div>
+                  )}
+            </div>
+          </div>
+
+          <div>
             <PillButton
-              variant='link'
-              size='sm'
-              className='text-xs font-medium text-orange hover:text-orange'
+              size='full'
+              onClick={handleRegister}
+              className='bg-orange hover:bg-orange text-black font-bold'
+              disabled={inputValue.trim() === ''}
             >
-              <LocationEditIcon className='w-3 h-3 mr-0' />
-              현재 위치로 설정
+              등록하기
             </PillButton>
           </div>
-        )}
-
-        {/* 입력 예시 (조건부 렌더링) */}
-        {inputValue.trim() !== '' && (
-          <div className='pt-4 space-y-3 text-sm pl-4'>
-            <p className='text-black font-bold'>입력예시</p>
-            <div>
-              <p className='text-secondary'>도로명 + 건물번호</p>
-              <p className='text-orange font-medium'>역삼로 180</p>
-            </div>
-            <div>
-              <p className='text-secondary'>지역명(동/리)+번지</p>
-              <p className='text-orange font-medium'>강남구 역삼동 790-6</p>
-            </div>
-            <div>
-              <p className='text-secondary'>지역명(동/리)+건물명</p>
-              <p className='text-orange font-medium'>역삼동 마루 180</p>
-            </div>
-          </div>
-        )}
-      </div>
-
-      {/* 등록하기 버튼 */}
-      <div className='px-6 pb-12 pt-8 z-10 mt-auto'>
-        <PillButton
-          size='full'
-          onClick={handleRegister}
-          className='bg-orange hover:bg-orange text-black font-bold'
-        >
-          등록하기
-        </PillButton>
-      </div>
-
-      {/* 등록 완료 모달 */}
-      {isRegistered && (
-        <div className='absolute inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50'>
-          <div className='bg-white px-6 py-4 rounded-xl flex items-center gap-4 shadow-lg'>
-            <CheckIcon className='text-primary-bg' />
-            <span className='text-black font-medium'>주소 등록을 완료했어요.</span>
-          </div>
         </div>
-      )}
-    </div>
+      </div>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              주소 등록을 완료했어요.
+            </AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction
+            // TODO: Move to the next page
+              onClick={() => setOpen(false)}
+            >
+              확인
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   )
 }
