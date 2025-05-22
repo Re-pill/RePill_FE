87d7b4e22f1c0ae4a907a6cdaa30fcb206d560b1
Helloyunho <me@helloyunho.xyz>
2025-05-23 01:18:45 +0900

    feat: actually finish find page

--- src/components/ui/map-marker.tsx
+++ src/components/ui/map-marker.tsx
@@ -1,16 +1,17 @@
 import { COLORFUL_MARKER, type ColorfulMarkerType } from '@/types/map'
 import { MapMarker } from 'react-kakao-maps-sdk'
 
-export function ColorfulMapMarker ({ type, ...props }: React.ComponentProps<typeof MapMarker> & {
+export function ColorfulMapMarker ({ type, bigger = false, ...props }: React.ComponentProps<typeof MapMarker> & {
   type: ColorfulMarkerType
+  bigger?: boolean
 }) {
   return (
     <MapMarker
       image={{
         src: COLORFUL_MARKER[type].src,
         size: {
-          width: 29,
-          height: 41
+          width: bigger ? 29 * 2 : 29,
+          height: bigger ? 41 * 2 : 41
         }
       }}
       {...props}
